dc81f5a20d9ebc7647ef3d0d11d150de11170da2
HaleyKlingenberg <haley@klingenberg.us>
2022-06-22 16:16:30 -0700

    First commit of my react app

---
+++ src/MyApp.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Table from './Table'
+function MyApp() {
+const characters = [
+    {
+    name: 'Charlie',
+    job: 'Janitor',
+    },
+    {
+    name: 'Mac',
+    job: 'Bouncer',
+    },
+    {
+    name: 'Dee',
+    job: 'Aspring actress',
+    },
+    {
+    name: 'Dennis',
+    job: 'Bartender',
+    },
+    ];
+
+
+return (
+<div className="container">
+<Table characterData={characters} />
+</div>
+);
+}
+export default MyApp;
